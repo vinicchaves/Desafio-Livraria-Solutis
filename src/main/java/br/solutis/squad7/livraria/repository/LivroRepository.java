ef5fb32b0dd8d49a5b088e2086f9ac3733cb40b7
Paulo Oliveira <111186037+p-oliveira7@users.noreply.github.com>
2023-08-13 05:49:34 -0300

    Update LivroRepository

--- src/main/java/br/solutis/squad7/livraria/repository/LivroRepository.java
+++ src/main/java/br/solutis/squad7/livraria/repository/LivroRepository.java
@@ -1,14 +1,21 @@
 package br.solutis.squad7.livraria.repository;
 
-import br.solutis.squad7.livraria.Livro;
-import br.solutis.squad7.livraria.livros.Impresso;
-import br.solutis.squad7.livraria.livros.Eletronico;
+import br.solutis.squad7.livraria.entity.Eletronico;
+import br.solutis.squad7.livraria.entity.Impresso;
 import org.springframework.data.jpa.repository.JpaRepository;
+import br.solutis.squad7.livraria.entity.Livro;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Long> {
-    List<Livro> findByTipo(String impresso);
+    List<Livro> findImpressosBy();
+
+    List<Livro> findEletronicosBy();
 }
+
