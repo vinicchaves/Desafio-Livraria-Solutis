fe7280dc07349b12e56654f4b12248122bcf24ae
Paulo Oliveira <111186037+p-oliveira7@users.noreply.github.com>
2023-08-13 06:06:02 -0300

    Update LivroRepository

--- src/main/java/br/solutis/squad7/livraria/repository/LivroRepository.java
+++ src/main/java/br/solutis/squad7/livraria/repository/LivroRepository.java
@@ -1,12 +1,7 @@
 package br.solutis.squad7.livraria.repository;
 
-import br.solutis.squad7.livraria.entity.Eletronico;
-import br.solutis.squad7.livraria.entity.Impresso;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.solutis.squad7.livraria.entity.Livro;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
